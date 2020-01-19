6ad0630749315011943ebdf3bf0ddd07e4814a5d
Orgnodor <max3@kth.se>
2020-01-19 18:37:49 +0100

    Should have set up the repo earlier... But now I have.

---
+++ MagicHealthCounter/Assets/PlayAreaUI.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayAreaUI : MonoBehaviour
+{
+    public TextMeshProUGUI playerHealthUI;
+    public TextMeshProUGUI commanderDamageUI;
+    public Image centerBackground;
+    public float centerFraction = 0.6f;
+    public float bottomFraction = 0.2f;
+
+    private int player = 0;
+    private int numPlayers = 0;
+    private RectTransform rect;
+    private int areaWidth;
+    private int areaHeight;
+    private int playerHealth = 40;
+    private int commanderDamage = 0;
+
+    public void FirstSetup()
+    {
+        transform.SetParent(FindObjectOfType<Canvas>().transform);
+        rect = GetComponent<RectTransform>();
+        SetupArea();
+
+        // Only needed on first setup
+        if ((player & 1) != 0)
+        {
+            Image img = GetComponent<Image>();
+            Color tmp = img.color;
+            tmp.a *= 0.5f;
+            img.color = tmp;
+        }
+    }
+
+    public void SetupArea()
+    {
+        RectTransform cbgrt = centerBackground.GetComponent<RectTransform>();
+
+        int layoutWidth = Mathf.CeilToInt(numPlayers / 2.0f);
+        areaWidth = Screen.width / layoutWidth;
+        areaHeight = Screen.height / 2;
+        int layoutX = (player - 1) % layoutWidth + 1;
+        int layoutY = player > layoutWidth ? 1 : 0;
+        if (layoutY == 1) // invert x order on the top
+            layoutX = layoutWidth + 1 - layoutX;
+
+        rect.sizeDelta = new Vector2(areaWidth, areaHeight);
+        cbgrt.sizeDelta = new Vector2(areaWidth * centerFraction, areaHeight);
+        rect.SetPositionAndRotation(new Vector3(areaWidth * (layoutX - 0.5f), areaHeight * (layoutY + 0.5f), 0.0f), Quaternion.Euler(0, 0, layoutY * 180));
+    }
+
+    public void SetValues(int health, int commanderDmg)
+    {
+        playerHealth = health;
+        commanderDamage = commanderDmg;
+
+        playerHealthUI.text = string.Format("{0}", playerHealth);
+        commanderDamageUI.text = string.Format("{0}", commanderDamage);
+    }
+
+    public void SetPlayer(int playerId)
+    {
+        player = playerId;
+    }
+
+    public void SetNumPlayers(int numberOfPlayers)
+    {
+        numPlayers = numberOfPlayers;
+    }
+
+    public bool ProcessClick(Vector2 screenPos)
+    {
+        bool hit = RectTransformUtility.RectangleContainsScreenPoint(rect, screenPos);
+
+        if (hit)
+        {
+            Vector3 localPos = rect.InverseTransformPoint(screenPos.x, screenPos.y, 0.0f);
+            int amount = Mathf.RoundToInt((Mathf.Abs(localPos.x) > areaWidth * centerFraction * 0.5f ? 5 : 1) * Mathf.Sign(localPos.x));
+
+            if (localPos.y > -bottomFraction * areaHeight)
+            {
+                playerHealth += amount;
+                playerHealthUI.text = string.Format("{0}", playerHealth);
+            }
+            else
+            {
+                commanderDamage += amount;
+                commanderDamageUI.text = string.Format("{0}", commanderDamage);
+            }
+        }
+
+        return hit;
+    }
+}
---
+++ MagicHealthCounter/Assets/UIManager.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject playerAreaPrefab;
+    public int numPlayers = 4;
+    public int startingHealth = 40;
+    public int startingCommanderDamage = 0;
+
+    private List<PlayAreaUI> playerAreas;
+    private bool clickBegan = true;
+
+    private GameObject options;
+    private RectTransform openOptions;
+    private int[] oValues = { 1, -1, 5, -5 };
+    private TextMeshProUGUI oHealth;
+    private List<RectTransform> oHealthPlusMinus;
+    private TextMeshProUGUI oCommanderDamage;
+    private List<RectTransform> oCommanderDamagePlusMinus;
+    private Toggle oCommanderDamageToggle;
+    private TextMeshProUGUI oPlayers;
+    private List<RectTransform> oPlayersPlusMinus;
+    private RectTransform oNewGame;
+    private RectTransform oBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setup the options page
+        options = GameObject.Find("Options");
+        options.SetActive(false);
+        openOptions = GameObject.Find("OpenOptions").GetComponent<RectTransform>();
+
+        // Health
+        oHealth = options.transform.Find("Health").GetComponent<TextMeshProUGUI>();
+        oHealth.text = string.Format("{0}", startingHealth);
+        oHealthPlusMinus = new List<RectTransform>();
+        oHealthPlusMinus.Add(oHealth.transform.Find("+1").GetComponent<RectTransform>());
+        oHealthPlusMinus.Add(oHealth.transform.Find("-1").GetComponent<RectTransform>());
+        oHealthPlusMinus.Add(oHealth.transform.Find("+5").GetComponent<RectTransform>());
+        oHealthPlusMinus.Add(oHealth.transform.Find("-5").GetComponent<RectTransform>());
+
+        // Commander Damage
+        oCommanderDamage = options.transform.Find("CommanderDamage").GetComponent<TextMeshProUGUI>();
+        oCommanderDamage.text = string.Format("{0}", startingCommanderDamage);
+        oCommanderDamagePlusMinus = new List<RectTransform>();
+        oCommanderDamagePlusMinus.Add(oCommanderDamage.transform.Find("+1").gameObject.GetComponent<RectTransform>());
+        oCommanderDamagePlusMinus.Add(oCommanderDamage.transform.Find("-1").gameObject.GetComponent<RectTransform>());
+        oCommanderDamagePlusMinus.Add(oCommanderDamage.transform.Find("+5").gameObject.GetComponent<RectTransform>());
+        oCommanderDamagePlusMinus.Add(oCommanderDamage.transform.Find("-5").gameObject.GetComponent<RectTransform>());
+        oCommanderDamageToggle = oCommanderDamage.transform.Find("Toggle").gameObject.GetComponent<Toggle>();
+        oCommanderDamageToggle.onValueChanged.AddListener(delegate
+        {
+            ToggleCommanderDamage(oCommanderDamageToggle);
+        });
+
+        // Players
+        oPlayers = options.transform.Find("Players").GetComponent<TextMeshProUGUI>();
+        oPlayers.text = string.Format("{0}", numPlayers);
+        oPlayersPlusMinus = new List<RectTransform>();
+        oPlayersPlusMinus.Add(oPlayers.transform.Find("+1").gameObject.GetComponent<RectTransform>());
+        oPlayersPlusMinus.Add(oPlayers.transform.Find("-1").gameObject.GetComponent<RectTransform>());
+
+        // New Game Button
+        oNewGame = options.transform.Find("NewGame").GetComponent<RectTransform>();
+
+        // Back Button
+        oBack = options.transform.Find("Back").GetComponent<RectTransform>();
+
+        playerAreas = new List<PlayAreaUI>();
+        CreatePlayerAreas();
+    }
+
+    void ResetPlayerAreas()
+    {
+        foreach (PlayAreaUI playArea in playerAreas)
+        {
+            playArea.SetValues(startingHealth, startingCommanderDamage);
+        }
+    }
+
+    void CreatePlayerAreas()
+    {
+        // Rearange previous areas
+        foreach (PlayAreaUI playArea in playerAreas)
+        {
+            playArea.SetNumPlayers(numPlayers);
+            playArea.SetupArea();
+        }
+
+        // Create new areas
+        for (int i = playerAreas.Count; i < numPlayers; i++)
+        {
+            GameObject playerArea = Instantiate(playerAreaPrefab);
+            PlayAreaUI ui = playerArea.GetComponent<PlayAreaUI>();
+            playerAreas.Add(ui);
+            ui.SetPlayer(i + 1);
+            ui.SetNumPlayers(numPlayers);
+            ui.SetValues(startingHealth, startingCommanderDamage);
+            ui.FirstSetup();
+
+            if (options.activeSelf)
+                ui.gameObject.SetActive(false);
+        }
+    }
+
+    void DestroyPlayerAreas()
+    {
+        for (int i = playerAreas.Count - 1; i > numPlayers - 1; i--)
+        {
+            Debug.LogFormat("playerAreas.Count: {0}, i: {1}", playerAreas.Count, i);
+            PlayAreaUI playerArea = playerAreas[i];
+            Destroy(playerArea.gameObject);
+            playerAreas.RemoveAt(i);
+        }
+
+        // Rearange remaning areas
+        foreach (PlayAreaUI playArea in playerAreas)
+        {
+            playArea.SetNumPlayers(numPlayers);
+            playArea.SetupArea();
+        }
+    }
+
+    void ToggleCommanderDamage(Toggle toggle)
+    {
+
+    }
+
+    void ToggleOptions(bool on)
+    {
+        foreach (PlayAreaUI playerArea in playerAreas)
+        {
+            playerArea.gameObject.SetActive(!on);
+        }
+        openOptions.gameObject.SetActive(!on);
+
+        options.SetActive(on);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && clickBegan)
+        {
+            Vector2 pos = Input.mousePosition;
+
+            if (options.activeSelf)
+            {
+                if (RectTransformUtility.RectangleContainsScreenPoint(oNewGame, pos))
+                {
+                    ResetPlayerAreas();
+                    ToggleOptions(false);
+                }
+                else if (RectTransformUtility.RectangleContainsScreenPoint(oBack, pos))
+                {
+                    ToggleOptions(false);
+                }
+                else
+                {
+                    int i = 0;
+                    foreach (RectTransform rect in oHealthPlusMinus)
+                    {
+                        if (RectTransformUtility.RectangleContainsScreenPoint(rect, pos))
+                        {
+                            startingHealth += oValues[i];
+                            oHealth.text = string.Format("{0}", startingHealth);
+                            return;
+                        }
+                        i++;
+                    }
+
+                    i = 0;
+                    foreach (RectTransform rect in oCommanderDamagePlusMinus)
+                    {
+                        if (RectTransformUtility.RectangleContainsScreenPoint(rect, pos))
+                        {
+                            startingCommanderDamage += oValues[i];
+                            oCommanderDamage.text = string.Format("{0}", startingCommanderDamage);
+                            return;
+                        }
+                        i++;
+                    }
+
+                    i = 0;
+                    foreach (RectTransform rect in oPlayersPlusMinus)
+                    {
+                        if (RectTransformUtility.RectangleContainsScreenPoint(rect, pos))
+                        {
+                            numPlayers += oValues[i];
+                            oPlayers.text = string.Format("{0}", numPlayers);
+                            if (oValues[i] > 0)
+                                CreatePlayerAreas();
+                            else
+                                DestroyPlayerAreas();
+                            return;
+                        }
+                        i++;
+                    }
+                }
+            }
+            else
+            {
+                if (RectTransformUtility.RectangleContainsScreenPoint(openOptions, pos))
+                {
+                    ToggleOptions(true);
+                }
+                else
+                {
+                    foreach (PlayAreaUI playerArea in playerAreas)
+                    {
+                        if (playerArea.ProcessClick(pos))
+                            return;
+                    }
+                }
+            }
+        }
+        else if (!Input.GetMouseButtonDown(0))
+            clickBegan = true;
+    }
+}
